2423bd71bb7fcae9dd21fc89aca4d0c20da08770
ClamSageCaleb <caleb.benjamin9799@gmail.com>
2020-05-29 11:20:43 -0400

    Added filtering capabilities to original file.

--- json-multi-csv.py
+++ json-multi-csv.py
@@ -2,11 +2,14 @@ import os
 import pandas as pd
 from pathlib import Path
 from flatten_json import flatten
+import re
 import json
 
-
+sDict = {}
 
 directory = input("Path to json dir: ")
+csvVertPath = r'./data-converted-vertical/'
+csvHorizPath = r'./data-converted-horizontal/'
 
 for filename in os.listdir(directory):
     if filename.endswith(".json"): 
@@ -15,19 +18,37 @@ for filename in os.listdir(directory):
         # read json
         with p.open('r', encoding='utf-8') as f:
             d = json.load(f)
-
-        # create dataframe
+        
         flat_json = (flatten(d))
+
+        # Selecting important elements from the flat_json dictionary
+        sDict['display_name'] = flat_json['display_name']
+        sDict['_primary_coach'] = flat_json['_primary_coach']
+        sDict['birth_date'] = flat_json['birth_date']
+        sDict['gender'] = flat_json['gender']
+        sDict['email_address'] = flat_json['email_address']
+
+        # Filtering the original dictionary by data and placing it in a new dictionary called sDict
+        for key in flat_json:
+            for m in re.finditer('related_PatientHealthResult_', key):
+                sDict[key] = flat_json[key]
+        
+        # create dataframe
         df = pd.DataFrame.from_dict(flat_json, orient="index")
+        dfFiltered = pd.DataFrame.from_dict(sDict, orient="index")
 
         # create excel (vertical) readable file
-        df.to_csv(root + '.csv', index=True, encoding="utf-8")
+        df.to_csv(csvVertPath + root + '.csv', index=True, encoding="utf-8")
+        dfFiltered.to_csv(csvVertPath + root + '(TEST).csv', index=True, encoding="utf-8")
 
         # create excel (horizontal) readable file
         df = pd.json_normalize(flat_json)
-        df.to_csv(root +'(format).csv', index=True, encoding="utf-8")
+        dfFiltered = pd.json_normalize(sDict)
+        df.to_csv(csvHorizPath + root +'(format).csv', index=True, encoding="utf-8")
+        dfFiltered.to_csv(csvHorizPath + root + '(TEST)(format).csv', index=True, encoding="utf-8")
 
-        print(root + ".csv is complete. " + root + "(format.csv) is complete.")
+        print("\n" + root + ".csv is complete. " + root + "(format.csv) is complete." +
+        "\nCheck the data-converted directories for your files.\n")
         continue
     else:
         continue
