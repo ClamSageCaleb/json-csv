61c10f850f9f71f2b7ab8138727e78ad4d29be00
ClamSageCaleb <caleb.benjamin9799@gmail.com>
2020-07-21 12:57:44 -0400

    Thinking about using the 'flat' package

--- json.ts
+++ json.ts
@@ -1,4 +1,5 @@
 var fs = require('fs');
+var flatten = require('flat');
 import {PythonShell} from 'python-shell';
 var dir = './jsons-for-py';
 
@@ -16,11 +17,12 @@ const arrayOfFiles = fs.readdirSync("./jsons")
 for (var i = 0; i < arrayOfFiles.length; i++) {
     var rawdata = fs.readFileSync('./jsons/' + arrayOfFiles[i]);
     var data = JSON.parse(rawdata);
+    //var flatdata = flatten(data);
     var fb = data.related.PatientHealthResult;
     for (var y = 0; y < fb.length; y++) {
         if(fb[y].occurred_at_local_time !== undefined && fb[y].data.value !== undefined && fb[y].data.value !== null) {
             var jsonData = `{"${arrayOfFiles[i]}":[{"Date ${y}": "${fb[y].occurred_at_local_time}", 
-            "Data ${y}": "${fb[y].data.value}"}]}`;
+            "Data ${y}": "${fb[y].data.value} ${fb[y].data.unit}"}]}`;
             var jsonObj = JSON.parse(jsonData);
             var jsonContents = JSON.stringify(jsonObj, null, 4);
             console.log(jsonContents);
