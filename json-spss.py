7dd6ed1458b815e20d6083392956047fa2f5df51
ClamSageCaleb <caleb.benjamin9799@gmail.com>
2020-08-03 12:17:16 -0400

    Renamed variables to be more easily read. Deleted json.ts and corresponding node_modules

--- json-spss.py
+++ json-spss.py
@@ -7,18 +7,35 @@ import re
 import json
 import time
 
-jsonDir = r'./jsons-for-py/'
+# Directories for the jsons and the converted xslx files
+jsonDir = r'./jsons/'
 jsonDone = r'./jsons-done/'
 csvPath = r'./data-converted/'
 
 def get_column_array(df, column):
+    '''
+    Drops empty values and empty cells.
+
+    :param DataFrame df: The dataframe to be modified
+    :param arr column: Column to be examined if empty 
+    :return: Returns the modified column array without empty values
+    '''
     expected_length = len(df)
+    # Drops empty values
     current_array = df[column].dropna().values
+    # Removes empty cells from the Dataframe
     if len(current_array) < expected_length:
         current_array = np.append(current_array, [''] * (expected_length - len(current_array)))
     return current_array
 
 def filtered(orig, ref):
+    '''
+    Renames keys and extracts important data from the original json
+
+    :param dict orig: The dictionary to be returned with the new/renamed data
+    :param dict ref: The reference dictionary that contains all of the json data
+    :return: The new dictionary with renamed keys and important data
+    '''
 
     fitbit = 'related_PatientHealthResult_'
 
@@ -26,38 +43,51 @@ def filtered(orig, ref):
     for key in ref:
         for m in re.finditer(fitbit, key):
             for n in re.finditer('_data_value', key):
+                # Renaming key from data_value to Value to be more easily called later.
                 orig[key.replace(fitbit, "Value ").replace("_data_value", "")] = ref[key]
 
             for n in re.finditer('_data_systolic_value', key):
+                # Renaming key from data_systolic_value to Systolic Value to be more easily called later.
                 orig[key.replace(fitbit, "Systolic Value ").replace("_data_systolic_value", "")] = ref[key]
 
             for n in re.finditer('_data_systolic_unit', key):
+                # Renaming key from data_systolic_unit to Systolic Unit Value to be more easily called later.
                 orig[key.replace(fitbit, "Systolic Unit ").replace("_data_systolic_unit", "")] = ref[key]
 
             for n in re.finditer('_data_diastolic_value', key):
+                # Renaming key from data_diastolic_value to Diastolic Value to be more easily called later.
                  orig[key.replace(fitbit, "Diastolic Value ").replace("_data_diastolic_value", "")] = ref[key]
                     
             for n in re.finditer('_data_diastolic_unit', key):
+                # Renaming key from data_diastolic_unit to Diastolic Unit to be more easily called later.
                 orig[key.replace(fitbit, "Diastolic Unit ").replace("_data_diastolic_unit", "")] = ref[key]
 
             for n in re.finditer('_data_unit', key):
+                # Renaming key from data_unit to Unit to be more easily called later.
                 orig[key.replace(fitbit, "Unit ").replace("_data_unit", "")] = ref[key]
                
             for n in re.finditer('_occurred_at_local_time', key):
+                # Renaming key from occured_at_local_time to Occured_At to be more easily called later.
                 orig[key.replace(fitbit, "Occurred_At ").replace("_occurred_at", "")] = ref[key]
             
             for n in re.finditer('_metric_type', key):
+                # Renaming key from metric_type to Metric to be more easily called later.
                 orig[key.replace(fitbit, "Metric ").replace("_metric_type", "")] = ref[key]
     
     return orig
 
 def dirs():
+    '''
+    Checks for directories and creates them if they don't exist
+    '''
+    # Checking if the needed directories exist
     if not os.path.exists(csvPath and jsonDir and jsonDone):
         print("\nCouldn't find directories: \n" + 
         "\t- jsons\n" + 
         "\t- data-conversion\n" +
         "\t- jsons-done\n" +
         "\nCreating directories now.")
+        # Creates directories
         os.makedirs(csvPath)
         os.makedirs(jsonDir)
         os.makedirs(jsonDone)
@@ -72,44 +102,67 @@ def dirs():
     print("\nBeginning conversion...\nTime: " + time.ctime() + "\n")
 
 def dfformat(df, dict):
+    '''
+    Iterates through the dictionary and matches dates in order to append data by day
+
+    :param DataFrame df: DataFrame that has the days listed as the column headers
+    :param dict dict: Dictionary containing the values that will be compared by date
+    :return: DataFrame with the appended data
+    '''
+
+    # Iterates through the days in the dataframe
     for day in df.columns:
+        # Iteratres through the keys in the dict
         for key in dict:
+            # Compares the date to the Occured_At key in the dictionary
             for n in re.finditer('Occurred_At ', key):
+                # Comapres by date instead of date and time
                 if day == dict[key].split('T')[0]:
+                    # Finds the key number from the dictionary in order to get matching data
                     num = re.findall(r'\d+', key)
                     numStr = ''.join(num)
-                    # TODO bug might be here, only finds value not Systolic Value or Diastolic Value
-                    val = "Value " + numStr
-                    sys = "Systolic Value " + numStr
-                    sunit = "Systolic Unit " + numStr
-                    dia = "Diastolic Value " + numStr
-                    dunit = "Diastolic Unit " + numStr
-                    unit = "Unit " + numStr
-                    metric = "Metric " + numStr
-                    if val in dict and unit in dict:
-                        x = dict[val]
-                        y = dict[unit]
-                        z = dict[metric]
-                        data = f"{x} {y} {z}"
-                        #data = str(x) + ' ' + y + ' ' + z
-                        df = df.append({day: data}, ignore_index=True)
-                    if sys in dict and sunit in dict:
-                        s = dict[sys]
-                        su = dict[sunit]
-                        z = dict[metric]
-                        dataTest = f"{s} {su} {z}"
-                        #dataTest = str(s) + ' ' + su + ' ' + metric
-                        df = df.append({day: dataTest}, ignore_index=True)
-                    if dia in dict and dunit in dict:
-                        d = dict[dia]
-                        du = dict[dunit]
-                        z = dict[metric]
-                        dataTestDia = f"{d} {du} {z}"
-                        #dataTestDia = str(d) + ' ' + du + ' ' + metric
-                        df = df.append({day: dataTestDia}, ignore_index=True)                 
+                    valNum = "Value " + numStr
+                    sysNum = "Systolic Value " + numStr
+                    sysUnitNum = "Systolic Unit " + numStr
+                    diaNum = "Diastolic Value " + numStr
+                    diaUnitNum = "Diastolic Unit " + numStr
+                    unitNum = "Unit " + numStr
+                    metricNum = "Metric " + numStr
+                    # Checks if the value is in the dictionary
+                    if valNum in dict and unitNum in dict:
+                        # Gets value from the key 
+                        stepWeight = dict[valNum]
+                        swUnit = dict[unitNum]
+                        swMetric = dict[metricNum]
+                        # Creates data and appeneds it to the dataframe
+                        swData = f"{stepWeight} {swUnit} {swMetric}"
+                        df = df.append({day: swData}, ignore_index=True)
+                    # Checks if the systolic value exists
+                    if sysNum in dict and sysUnit in dict:
+                        # Gets systolic value from key
+                        sysVal = dict[sysNum]
+                        sysUnit = dict[sysUnitNum]
+                        sysMetric = dict[metricNum]
+                        # Creates data and appeneds it to the dataframe
+                        sysData = f"{sysVal} {sysUnit} {sysMetric}"
+                        df = df.append({day: sysData}, ignore_index=True)
+                    # Checks if the diastolic value exists
+                    if diaNum in dict and diaUnit in dict:
+                        # Gets diastolic value from key
+                        diaVal = dict[diaNum]
+                        diaUnit = dict[diaUnit]
+                        diaMetric = dict[metricNum]
+                        # Creates data and appeneds it to the dataframe
+                        diaData = f"{diaVal} {diaUnit} {diaMetric}"
+                        df = df.append({day: diaData}, ignore_index=True)                 
     return df
 
 def js_output():
+    '''
+    Returns output to show user that data conversion is happening
+
+    :return: output with file name and location
+    '''
     output = "\n" + root + ".xlsx is complete. " +  "\nCheck the data-converted directories for your files.\n" + "Moving " + filename + " -> jsons-done.\n"
     print(output)
 
@@ -122,7 +175,7 @@ for filename in os.listdir(jsonDir):
         p = Path(jsonDir + "/" + filename)
         root, ext = os.path.splitext(filename)
 
-        # read json
+        # Read json files
         with p.open('r', encoding='utf-8') as f:
             d = json.load(f)
         flat_json = (flatten(d))
@@ -130,14 +183,16 @@ for filename in os.listdir(jsonDir):
 
         sDict = filtered(sDict, flat_json)
        
+        # Takes value from Occurred_At key and places the value in a dictionary of days
         for key, v in sDict.items():
             for n in re.finditer('Occurred_At', key):
                 date = v.split('T')[0]
                 days[date] = None
         
+        # Sorts the days dictionary
         sDays = sorted(days.keys())
 
-        # create dataframe
+        # Create dataframe
         df = pd.DataFrame(columns=list(sDays))
 
         df = dfformat(df, sDict)
